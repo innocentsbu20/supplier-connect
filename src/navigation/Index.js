60f941a92f4738858c3bed792e83d40c9f882873
minnieT <minenhlethabiso310@gmail.com>
2023-08-11 22:21:34 +0200

    Created error 404 page

--- src/navigation/Index.js
+++ src/navigation/Index.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Main from "../views/main/Main";
 import Login from "../views/auth/login/Login";
 import Register from "../views/auth/register/Register";
+import NotFound from "../views/main/not-found/NotFound";
 
 const Dashboard = () => {
   return (
@@ -9,6 +10,7 @@ const Dashboard = () => {
       <Route path="/" element={<Main />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
