797721373c2ca88ee2372c29de6dfb96f29554cf
minnieT <minenhlethabiso310@gmail.com>
2023-08-12 19:46:42 +0200

    Created button group

---
+++ src/components/button-group/ButtonGroup.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+
+export default function ConnectButtonGroup({ onChange }) {
+
+    const [state, setState] = useState({
+        activeToggle: 'All Inventory'
+    });
+
+    const handleChange = (event, newAlignment) => {
+        onChange(newAlignment)
+        console.log("newAlignment ", newAlignment)
+        setState({
+            ...state,
+            activeToggle: newAlignment
+        });
+    };
+    const { activeToggle } = state;
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                '& > *': {
+                    my: 4,
+                },
+            }}
+        >
+            <ToggleButtonGroup
+                color="standard"
+                value={activeToggle}
+                exclusive
+                style={{ color: 'red' }}
+                onChange={handleChange}
+                aria-label="Platform"
+            >
+                <ToggleButton value="All Inventory">All Inventory</ToggleButton>
+                <ToggleButton value="mobile">Mobile</ToggleButton>
+                <ToggleButton value="tablet">Tablets</ToggleButton>
+                <ToggleButton value="laptop">Laptops</ToggleButton>
+                <ToggleButton value="computer">Desktop Computers</ToggleButton>
+            </ToggleButtonGroup>
+        </Box >
+    );
+}
