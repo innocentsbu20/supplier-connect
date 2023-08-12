c970733072bf5e928b20e734f4a3df7cd23bf007
minnieT <minenhlethabiso310@gmail.com>
2023-08-12 15:38:36 +0200

    Added navigation on landing/login page

--- src/views/auth/login/Login.js
+++ src/views/auth/login/Login.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import './../Index.scss';
 import {
-    Box, Paper, Typography, TextField, Avatar,
+    Box, Paper, Typography, TextField,
     ToggleButton, ToggleButtonGroup, Button
 } from '@mui/material';
-import logo from './../../../assets/logo.svg';
 import { useNavigate } from "react-router-dom";
 import ImageLogo from '../../../components/svg-image/ImageLogo';
+import Header from '../../../components/header/Header';
 
 export default function Login() {
     const [state, setState] = useState({
@@ -24,85 +24,86 @@ export default function Login() {
     const navigate = useNavigate();
 
     const { username, password, type } = state;
+    const menuItems = ['Products', 'About Us', 'Contact/Support', 'Blog/News'];
 
     return (
-
-        <div className='auth'>
-            <div className='login-form'>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        '& > :not(style)': {
-                            m: 1,
-                            width: 500,
-                        },
-                    }}
-                >          <Paper elevation={9} className='content'  >
-                        <Box justifyContent='center' alignItems='center'>
-                            <ImageLogo />
-                            <Typography variant='h3' ml={1} fontFamily='sans-serif' fontWeight='bold' component='h2'>
-                                Login
-                            </Typography>
-                        </Box>
-                        <Box my={3}>
-                            <Box
-                                component="form"
-                                sx={{
-                                    '& .MuiTextField-root': { m: 1, width: '80%' },
-                                }}
-                                noValidate
-                                autoComplete="off"
-                            >
-                                <div style={{}}>
-                                    <TextField
-                                        required
-                                        fullWidth
-                                        id="username"
-                                        label="Username"
-                                        defaultValue={username}
-                                    />
-                                    <ToggleButtonGroup
-                                        color="standard"
-                                        value={type}
-                                        exclusive
-                                        onChange={handleChange}
-                                        aria-label="Platform"
-                                    >
-                                        <ToggleButton value="Buyer">Buyer</ToggleButton>
-                                        <ToggleButton value="Supplier">Supplier</ToggleButton>
-                                        <ToggleButton value="Both" disabled>Both</ToggleButton>
-                                    </ToggleButtonGroup>
-                                    <TextField
-                                        id="outlined-password-input"
-                                        label="Password"
-                                        fullWidth
-                                        type="password"
-                                        autoComplete="current-password"
-                                        defaultValue={password}
-                                    />
-                                </div>
-                            </Box>
-                            <Box display='flex' py={1} px={6} justifyContent='flex-end' alignItems='center'>
-                                <Typography onClick={() => navigate("/forgot")} className='pointer' variant='h6' component='h6'>
-                                    forgot password?
+        <>
+            <Header isLogin menuItems={menuItems} />
+            <div className='auth'>
+                <div className='login-form'>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            '& > :not(style)': {
+                                m: 1,
+                                width: 500,
+                            },
+                        }}
+                    >          <Paper elevation={9} className='content'  >
+                            <Box justifyContent='center' alignItems='center'>
+                                <ImageLogo />
+                                <Typography variant='h3' ml={1} fontFamily='sans-serif' fontWeight='bold' component='h2'>
+                                    Login
                                 </Typography>
                             </Box>
-                            <Box display='flex' px={6} justifyContent='flex-end' alignItems='center'>
-                                <Button onClick={() => navigate("/")} variant="outlined" color={'inherit'}>Login</Button>
+                            <Box my={3}>
+                                <Box
+                                    component="form"
+                                    sx={{
+                                        '& .MuiTextField-root': { m: 1, width: '80%' },
+                                    }}
+                                    noValidate
+                                    autoComplete="off"
+                                >
+                                    <div style={{}}>
+                                        <TextField
+                                            required
+                                            fullWidth
+                                            id="username"
+                                            label="Username"
+                                            defaultValue={username}
+                                        />
+                                        <ToggleButtonGroup
+                                            color="standard"
+                                            value={type}
+                                            exclusive
+                                            onChange={handleChange}
+                                            aria-label="Platform"
+                                        >
+                                            <ToggleButton value="Buyer">Buyer</ToggleButton>
+                                            <ToggleButton value="Supplier">Supplier</ToggleButton>
+                                            <ToggleButton value="Both" disabled>Both</ToggleButton>
+                                        </ToggleButtonGroup>
+                                        <TextField
+                                            id="outlined-password-input"
+                                            label="Password"
+                                            fullWidth
+                                            type="password"
+                                            autoComplete="current-password"
+                                            defaultValue={password}
+                                        />
+                                    </div>
+                                </Box>
+                                <Box display='flex' py={1} px={6} justifyContent='flex-end' alignItems='center'>
+                                    <Typography onClick={() => navigate("/forgot")} className='pointer' variant='h6' component='h6'>
+                                        forgot password?
+                                    </Typography>
+                                </Box>
+                                <Box display='flex' px={6} justifyContent='flex-end' alignItems='center'>
+                                    <Button onClick={() => navigate("/")} variant="outlined" color={'inherit'}>Login</Button>
+                                </Box>
+                                <Box display='flex' className='pointer' justifyContent='center' alignItems='center'>
+                                    <Typography onClick={() => navigate("/register")} variant='button' component='h2'>
+                                        create account
+                                    </Typography>
+                                </Box>
                             </Box>
-                            <Box display='flex' className='pointer' justifyContent='center' alignItems='center'>
-                                <Typography onClick={() => navigate("/register")} variant='button' component='h2'>
-                                    create account
-                                </Typography>
-                            </Box>
-                        </Box>
-                    </Paper>
-                </Box>
+                        </Paper>
+                    </Box>
+                </div >
             </div >
-
-        </div >
-
+        </>
     )
 
 }
